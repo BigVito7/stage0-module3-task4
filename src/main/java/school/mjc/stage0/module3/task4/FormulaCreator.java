23c64829bb6094dc23445eb966eabc586f536028
BigVito7 <biggvito7@gmail.com>
2025-02-25 12:39:22 +0200

    formula

--- src/main/java/school/mjc/stage0/module3/task4/FormulaCreator.java
+++ src/main/java/school/mjc/stage0/module3/task4/FormulaCreator.java
@@ -2,5 +2,14 @@ package school.mjc.stage0.module3.task4;
 
 public class FormulaCreator {
     public static void main(String[] args) {
+        double a = 9; // Represents the variable a
+        double b = 5; // Represents the variable b
+
+
+        // Calculate the formula
+        double result = (9 * Math.pow(a, 2) - 5 * b + 2 + a - 7) * ((a + b - 4 * a * b) / 2);
+
+        // Print the result
+        System.out.println("The result is: " + result);
     }
 }
